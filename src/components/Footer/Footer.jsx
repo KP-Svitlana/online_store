bb9b35dea6f17c9bc0284cac83aaf6354d6ca15e
ОlexaEvtush <olexaevtush@gmail.com>
2023-07-20 14:55:49 +0300

    add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,107 @@
-export const Footer = ({}) => {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { StyledFooter, StyledFooterMenu, StyledFooterLinks, StyledFooterImage, StyledFooterContacts, StyledFooterAddress, StyledFooterAddressText,StyledFooterPhone,StyledFooterPhoneNum,StyledFooterEmail,StyledFooterEmailExa,StyledFooterTime,StyledFooterNav,StyledFooterShop,StyledFooterShopTitle,StyledFooterMenuItem,StyledFooterItemMenu,StyledFooterCompany,StyledFooterHelp,StyledFooterLine,StyledLineFooter,StyledFooterCopy,StyledFooterLink,StyledFooterPrivacy,StyledFooterTerms  } from "./Footer.styled";
+import logoImage from './icons/Logo.png';
+
+export const Footer = () => {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <StyledFooter>
+      <StyledFooterMenu>
+        <StyledFooterLinks>
+          <StyledFooterImage src={logoImage} alt="Logo"/>
+        </StyledFooterLinks>
+        <StyledFooterContacts>
+          <StyledFooterAddress>
+            <StyledFooterAddressText>Київ, вул. Шевченка, 21</StyledFooterAddressText>
+          </StyledFooterAddress>
+          <StyledFooterPhone>
+            <StyledFooterPhoneNum>+38 (800) 000-000</StyledFooterPhoneNum>
+          </StyledFooterPhone>
+          <StyledFooterEmail>
+            <StyledFooterEmailExa>example@gmail.com</StyledFooterEmailExa>
+          </StyledFooterEmail>
+          <StyledFooterTime>пн-пт: з 9:00 до 18:00 <br />сб-нд: з 10:00 до 17:00</StyledFooterTime>
+        </StyledFooterContacts>
+      </StyledFooterMenu>
+      <StyledFooterNav>
+        <StyledFooterShop>
+          <StyledFooterShopTitle>МАГАЗИН</StyledFooterShopTitle>
+          <StyledFooterMenuItem>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Новинки</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Каталог</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Колекції</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Взуття</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Аксесуари</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Розпродаж</NavLink>
+            </StyledFooterItemMenu>
+          </StyledFooterMenuItem>
+        </StyledFooterShop>
+        <StyledFooterCompany>
+          <StyledFooterShopTitle>КОМПАНІЯ</StyledFooterShopTitle>
+          <StyledFooterMenuItem>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Про нас</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Контакти</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Відгуки</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Оферта</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Блог</NavLink>
+            </StyledFooterItemMenu>
+          </StyledFooterMenuItem>
+        </StyledFooterCompany>
+        <StyledFooterHelp>
+          <StyledFooterShopTitle>ДОПОМОГА</StyledFooterShopTitle>
+          <StyledFooterMenuItem>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Оформлення замовлень</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Доставка і оплата</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Гарантія та повернення</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Умови акцій</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">FAQ</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Terms&amp;Conditions</NavLink>
+            </StyledFooterItemMenu>
+            <StyledFooterItemMenu>
+              <NavLink to="#" activeClassName="active">Політика конфіденційності</NavLink>
+            </StyledFooterItemMenu>
+          </StyledFooterMenuItem>
+        </StyledFooterHelp>
+      </StyledFooterNav>
+      <StyledFooterLine>
+        <StyledLineFooter></StyledLineFooter>
+        <StyledFooterCopy>©2023 Logo All right reserved</StyledFooterCopy>
+        <StyledFooterLink>
+          <StyledFooterPrivacy>Privacy Policy</StyledFooterPrivacy>
+          <StyledFooterTerms>Terms of Service</StyledFooterTerms>
+        </StyledFooterLink>
+      </StyledFooterLine>
+    </StyledFooter>
   );
 };
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,197 @@
+import styled from "@emotion/styled";
+
+export const StyledFooter = styled.footer`
+    display: flex;
+    flex-direction: row;
+    height: 558px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    font-size: 14px;
+    align-self: stretch;
+    width: 100%;
+    min-height: 100vh;
+    padding: 120px 0 20px 0;
+    text-decoration: none;
+    color: inherit;
+`;
+export const StyledFooterMenu = styled.div`
+    display: flex;
+    gap: 36px;
+    align-items: flex-start;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+    flex-wrap: wrap;
+    flex-direction: column;
+    @media (max-width: 768px) {
+        flex-direction: row;
+        justify-content: flex-start;
+        gap: 36px;
+    }
+`;
+export const StyledFooterLinks = styled.a`
+    text-align: left;
+    text-decoration: none;
+    color: inherit;
+    position: relative;
+`;
+export const StyledFooterImage = styled.img`
+`;
+export const StyledFooterContacts = styled.address`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    align-items: flex-start;
+    justify-content: center;
+    line-height: 1.3;
+`;
+export const StyledFooterAddress = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    align-items: center;
+    justify-content: center;
+`;
+export const StyledFooterAddressText = styled.p`
+    text-align: left;
+`;
+export const StyledFooterPhone = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+`;
+export const StyledFooterPhoneNum = styled.p`
+    text-align: left;
+`;
+export const StyledFooterEmail = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+`;
+export const StyledFooterEmailExa = styled.p`
+    text-align: left;
+`;
+export const StyledFooterTime = styled.div`
+    text-align: left;
+`;
+export const StyledFooterNav = styled.nav`
+    display: flex;
+    flex-direction: row;
+    gap: 86px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-shrink: 0;
+    flex-wrap: wrap;
+    margin-left: auto;
+    @media (max-width: 768px) { 
+        margin-top: auto;
+        margin-left: 0;
+    }
+`;
+export const StyledFooterShop = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-shrink: 0;
+    gap: 24px;
+`;
+export const StyledFooterShopTitle = styled.h1`
+    text-align: left;
+    font-weight: 600;
+    line-height: 1.3;
+    font-size: 14px;
+`;
+export const StyledFooterMenuItem = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    align-items: flex-start;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+    font-weight: 400;
+    line-height: 1.5;
+    font-size: 14px;
+`;
+export const StyledFooterItemMenu = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+export const StyledFooterCompany = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    align-items: flex-start;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+`;
+export const StyledFooterHelp = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    align-items: flex-start;
+    justify-content: center;
+    flex-shrink: 0;
+    position: relative;
+`;
+export const StyledFooterLine = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-top: auto;
+    width: 100%;
+    position: relative;
+`;
+export const StyledLineFooter = styled.div`
+    border-style: solid;
+    border-color: #515151;
+    border-width: 1px 0 0 0;
+    flex-shrink: 0;
+    width: 100%;
+    position: relative;
+    margin-bottom: 24px;
+`;
+export const StyledFooterCopy = styled.div`
+    color: #515151;
+    text-align: left;
+    position: relative;
+`;
+export const StyledFooterLink = styled.ul`
+    display: flex;
+    flex-direction: row;
+    gap: 32px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    position: relative;
+    list-style-type: none;
+`;
+export const StyledFooterPrivacy = styled.li`
+    color: #515151;
+    text-align: left;
+    position: relative;
+`;
+export const StyledFooterTerms = styled.li`
+    color: #515151;
+    text-align: left;
+    position: relative;
+`;
