2bd25ed4eca0ef2c42bf6ba465ca8684f9b6f156
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-07-22 14:13:59 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,22 @@
-import {Localisation} from "../Localisation/Localisation";
-import {Logo} from "../Logo/Logo";
-import { UserNav } from "../UserNav/UserNav";
-import { FilterNav } from "../FilterNav/FilterNav";
-import { SearchPanel } from "../SearcPanel/SearcPanel";
-import { 
-  HeaderWrap, 
-  HeaderBlockUser,
-  HeaderNavApp 
-} from "./Header.styled";
+import { Localisation } from "../Localisation";
+import { Logo } from "../Logo/Logo";
+import { UserNav } from "../UserNav";
+import { FilterNav } from "../FilterNav";
+import { SearchPanel } from "../SearchPanel";
+import { HeaderWrap, HeaderBlockUser, HeaderNavApp } from "./Header.styled";
 
 export const Header = ({}) => {
   return (
-      <HeaderWrap>
-        <HeaderBlockUser>
-          <Localisation />
-          <Logo />
-          <UserNav />
-        </HeaderBlockUser>
-        <HeaderNavApp>
-            <FilterNav />
-            <SearchPanel />
-        </HeaderNavApp>
-      </HeaderWrap>
+    <HeaderWrap>
+      <HeaderBlockUser>
+        <Localisation />
+        <Logo />
+        <UserNav />
+      </HeaderBlockUser>
+      <HeaderNavApp>
+        <FilterNav />
+        <SearchPanel />
+      </HeaderNavApp>
+    </HeaderWrap>
   );
 };
