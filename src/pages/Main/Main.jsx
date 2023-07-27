2ee92989b1305b7283d22e8919252793acb73bc2
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-07-27 16:18:27 +0300

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,41 +1,40 @@
-import {NavLink} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import {ProductsList} from "../../components/ProductsList/index.js";
-
+import { ProductsList } from "../../components/ProductsList/index.js";
+import { Categories } from "../../components/Categories";
 
 import axios from "axios";
-axios.defaults.baseURL = 'https://64934ce0428c3d2035d1a156.mockapi.io/api/v1';
+axios.defaults.baseURL = "https://64934ce0428c3d2035d1a156.mockapi.io/api/v1";
 const getAllProducts = async () => {
-    try {
-        const {data} = await axios.get('/users');
-        return data;
-    } catch (err) {
-        console.log(err.message);
-    }
+  try {
+    const { data } = await axios.get("/users");
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-
 const Main = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getAllProducts().then((res) => setData(res));
+  }, []);
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        getAllProducts().then(res => setData(res));
-    }, []);
+  return (
+    <>
+      <div>
+        <h1>Main Page</h1>
+        <NavLink to="/login" style={{ marginRight: "10px" }}>
+          Login
+        </NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+      </div>
 
-    return (
-        <>
-            <div>
-                <h1>Main Page</h1>
-                <NavLink to="/login" style={{marginRight: "10px"}}>
-                    Login
-                </NavLink>
-                <NavLink to="/signup">Signup</NavLink>
-            </div>
-
-            <ProductsList products={data}/>
-        </>
-    );
+      <ProductsList products={data} />
+      <Categories />
+    </>
+  );
 };
 
 export default Main;
