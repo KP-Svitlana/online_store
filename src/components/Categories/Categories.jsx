5b3dd1504bc86bd4a246f5e92e2d710d6791611a
ОlexaEvtush <olexaevtush@gmail.com>
2023-07-28 16:11:24 +0300

    Update categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,31 +1,70 @@
 import React from 'react';
-import { StyledCategories, CategoriesText, CategoriesPhotoRightWrap, CategoriesWrap, CategoriesPhotoUpContainer, CategoriesLink, CategoriesPhotoLeft, CategoriesPhotoDown, CategoriesPhotoUpLeft, CategoriesPhotoUpRight } from "./Categories.styled";
+import { useState, useEffect } from 'react';
+import {
+  StyledCategories,
+  CategoriesText,
+  CategoriesPhotoRightWrap,
+  CategoriesWrap,
+  CategoriesPhotoUpContainer,
+  CategoriesLink,
+  CategoriesPhotoLeft,
+  CategoriesPhotoDown,
+  CategoriesPhotoUpLeft,
+  CategoriesPhotoUpRight
+} from "./Categories.styled";
 import imageLeft from "../../assets/img/CategoriesLeft.png";
 import imageUpLeft from "../../assets/img/CategoriesUpLeft.png";
 import imageUpRight from "../../assets/img/CategoriesUpRight.png";
 import imageDown from "../../assets/img/CategoriesRightDown.png";
 
+const getNumOfPhotosToDisplay = (windowWidth) => {
+  return windowWidth <= 1440 ? 1 : 2;
+};
 
 export const Categories = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [numOfPhotosToDisplay, setNumOfPhotosToDisplay] = useState(getNumOfPhotosToDisplay(windowWidth));
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setNumOfPhotosToDisplay(getNumOfPhotosToDisplay(windowWidth));
+  }, [windowWidth]);
+
+  const renderCategoriesPhotos = () => {
+    const photosToDisplay = [imageLeft, imageUpLeft, imageUpRight, imageDown].slice(0, numOfPhotosToDisplay);
+
+    return photosToDisplay.map((photo, index) => (
+      <CategoriesLink to="#" key={index}>
+        <CategoriesPhotoLeft src={photo} alt={`Categories_${index + 1}`} />
+      </CategoriesLink>
+    ));
+  };
+
   return (
     <StyledCategories>
       <CategoriesText>Категорії</CategoriesText>
       <CategoriesWrap>
-        <CategoriesLink href="#">
-          <CategoriesPhotoLeft src={imageLeft} alt="Categories_1" />
-        </CategoriesLink>
+        {renderCategoriesPhotos()}
         <CategoriesPhotoRightWrap>
-
           <CategoriesPhotoUpContainer>
-            <CategoriesLink>
+            <CategoriesLink to="#">
               <CategoriesPhotoUpLeft src={imageUpLeft} alt="Categories_2" />
             </CategoriesLink>
-            <CategoriesLink href="#">
+            <CategoriesLink to="#">
               <CategoriesPhotoUpRight src={imageUpRight} alt="Categories_3" />
             </CategoriesLink>
           </CategoriesPhotoUpContainer>
-
-          <CategoriesLink href="#">
+          <CategoriesLink to="#">
             <CategoriesPhotoDown src={imageDown} alt="Categories_4" />
           </CategoriesLink>
         </CategoriesPhotoRightWrap>
--- src/components/Categories/Categories.styled.js
+++ src/components/Categories/Categories.styled.js
@@ -4,37 +4,42 @@ import { device } from "../../styles/mediaVeriables";
 import { Link } from "react-router-dom";
 
 export const StyledCategories = styled.div`
-  padding: 80px 0;
-
-  @media ${device.tabletBefore} {
-    padding: 32px 0;
+  padding: 80px 0; 
+  @media ${device.tablet} {
+    display: flex;
+    flex-direction: column;
   }
-  @media ${device.desktop} {
-    padding: 80px 0;
+  @media ${device.mobileTablet} {
+    padding: 0;
   }
-`; 
-
+`;
 export const CategoriesLink = styled(Link)`
 `;
 export const CategoriesText = styled.p`
   color: ${paletteGray.text};
-  font-family: "Gothic A1", sans-serif;
   font-size: 24px;
   font-weight: 500;
   line-height: 1.3;
   letter-spacing: 2px;
   text-transform: uppercase;
   margin-bottom: 32px;
+  @media ${device.mobileTablet} {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1.2;
+  }
 `;
 export const CategoriesWrap = styled.div`
   display: flex;
   gap: 20px;
-  flex-wrap: wrap;
-  position: relative;
   justify-content: center;
   align-items: flex-start;
-  @media ${device.tablet} {
-
+  
+  @media ${device.mobileOnly} {
+    gap: 12px;
+  }
+  @media ${device.mobileTablet} {
+    gap: 12px;
   }
 `;
 
@@ -43,31 +48,72 @@ export const CategoriesPhotoLeft = styled.img`
   flex-direction: row;
   width: 343px;
   @media ${device.tablet} {
-    
+    width: 344px;
+  }
+  @media ${device.tabletBefore} {
+    width: 344px;
+  }
+  @media ${device.mobileOnly} {
+    width: 173px;
+    height: 220px;
   }
 `;
 export const CategoriesPhotoDown = styled.img`
   display: flex; 
   flex-direction: row;
   width: 700px;
+  @media ${device.mobileOnly} {
+    width: 358px;
+    height: 240px;
+  }
+  @media ${device.mobileTablet} {
+    width: 100%;
+  }
 `;
 export const CategoriesPhotoUpContainer= styled.div`
   display: flex;
   flex-direction: row;
   gap: 20px;
-  @media ${device.tablet} {
-
+  @media ${device.tabletBefore} {
+    gap: 16px;
   }
+  @media ${device.mobileOnly} {
+    display: flex; 
+    flex-direction: column;
+    gap: 12px;
+  }
+  @media ${device.mobileTablet} {
+    gap: 12px;
+  }
+  
 `;
 export const CategoriesPhotoRightWrap = styled.div`
   display: flex; 
   flex-direction: column;
   gap: 20px;
   width: 700px;
+  @media ${device.tabletBefore} {
+    gap: 16px;
+  }
+  @media ${device.mobileTablet} {
+    gap: 12px;
+  }
 `;
 export const CategoriesPhotoUpLeft = styled.img`
-  width: 413px;
+  @media ${device.desktopBefore} {
+    width: 413px;
+  }
+  @media ${device.mobileOnly} {
+    width: 173px;
+    height: 104px;
+  }
 `;
 export const CategoriesPhotoUpRight = styled.img`
-  width: 413px;
+  @media ${device.desktopBefore} {
+    width: 413px;
+  }
+  @media ${device.mobileOnly} {
+    width: 173px;
+    height: 104px;
+  }
 `;
